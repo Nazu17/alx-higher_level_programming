c46a037d8da37a94b0ee4fd07751c8f47aebe31f
Nazrawi Asrat <nasrat2676@gmail.com>
2023-10-30 12:28:55 -0700

    A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-
-"""
-More Classes and Objects Python
-"""
+"""Rectangle module"""
 
 
 class Rectangle:
-    """
-    The class Rectangle
-    """
+    """ Rectangle class"""
     number_of_instances = 0
+    """int: number of instances"""
+
     print_symbol = "#"
+    """any type: symbol for printing rectangle"""
 
     def __init__(self, width=0, height=0):
-        """__init__
-        Arguments:
-            width : width of rectangle
-            height : height of rectangle
-        """
+        """Initialiazes the rectangle"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
-        """width"""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        """width
-            value: new width"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -39,58 +30,54 @@ class Rectangle:
 
     @property
     def height(self):
-        """height"""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        """height
-            value: new height"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """area"""
+        """Returns the area of the rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """perimeter"""
-        if self.__width == 0 or self.height == 0:
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
-
-    def __repr__(self):
-        """__repr__"""
-        return f"Rectangle({self.__width}, {self.__height})"
+        return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        """__str__"""
+        """Returns a string rep of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join([str(self.print_symbol) * self.__width] *
-                         self.__height)
+        return ((str(self.print_symbol) * self.__width + "\n") *
+                self.__height)[:-1]
+
+    def __repr__(self):
+        '''Returns formal string representation...'''
+        return "Rectangle(" + str(self.width) + ", " + str(self.height) + ")"
 
     def __del__(self):
-        """__del__"""
+        """Called when deleting an instance of rectangle"""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        else:
-            if rect_1.area() > rect_2.area():
-                return rect_1
-            elif rect_1.area() < rect_2.area():
-                return rect_2
-            else:
-                return rect_1
+        """returns the biggest of the two rectangles"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if type(rect_2) is not Rectangle:
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        return rect_1
 
     @classmethod
     def square(cls, size=0):
+        """Return a new instance of rec with dimensions == size"""
         return cls(size, size)
