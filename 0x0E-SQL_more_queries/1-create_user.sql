25cace34c4fb616c230de756fb1a9adbadf867ae
Nazrawi Asrat <nasrat2676@gmail.com>
2023-12-13 12:23:42 -0800

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- script that creates the user user_0d_1.
--- If the user user_0d_1 already exists, your script should not fail.
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- a script that creates the MySQL server user user_0d_1.
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
