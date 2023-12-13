e9ea83d380d360208371474d81ee9da74e4c1d5d
Nazrawi Asrat <nasrat2676@gmail.com>
2023-12-13 12:27:25 -0800

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2.
--- If the user user_0d_2 or hbtn_0d_2 database already exists, your script should not fail.
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd'
+GRANT SELECT on hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- CREATE DATABASE IF NOT EXISTS hbtn_0d_2
